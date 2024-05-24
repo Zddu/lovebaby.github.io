0c0e85eaa09077b102426f26d0fd5b20c63b69b8
zdde <1501815524@qq.com>
2024-05-25 00:06:14 +0800

    feat: 新增一个模块

---
+++ common/hm.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || []
+;(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?765034d8d60f49b8b0c1e501d339b435'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
